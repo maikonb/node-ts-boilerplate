896e8ed607cf10613d2065ab3d9d3425263a4813
maikon <maikon@gmail.com>
2020-12-26 19:51:16 -0300

    Add express tsc babel

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `Server started at http://localhost:${ port }` );
+});
